c82be2d21501f5504b5760378feacdba8071911f
rubenispiryan <ruben.ispir@gmail.com>
2026-02-25 17:55:43 -0500

    moves log to /tmp

--- src/prompt.py
+++ src/prompt.py
@@ -18,7 +18,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler('experiment.log'),
+        logging.FileHandler('/tmp/experiment.log'),
         logging.StreamHandler()
     ]
 )
